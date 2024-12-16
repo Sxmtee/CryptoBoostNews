9a86b91db9d16411dcd793ea681202a79808ede5
sxmtee <somtoo76@gmail.com>
2024-12-16 19:42:46 +0100

    Initial Commit

---
+++ CryptoBoost/CryptoBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBoostApp.swift
+//  CryptoBoost
+//
+//  Created by mac on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
